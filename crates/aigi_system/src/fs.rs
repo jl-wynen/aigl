43d6e85ed1cfe08c8548204c131547e64f4f9604
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-03-23 17:30:11 +0100

    Also check if dir

--- crates/aigi_system/src/fs.rs
+++ crates/aigi_system/src/fs.rs
@@ -1,9 +1,16 @@
-use anyhow::{bail, Context};
+use anyhow::{Context, bail};
 use std::path::Path;
 
 pub fn create_output_directory(path: &Path) -> anyhow::Result<()> {
-    if path.exists() && !directory_is_empty(path) {
-        bail!("Output directory is not empty: {}", path.display());
+    if path.exists() {
+        if !path.is_dir() {
+            bail!(
+                "Output path exists and is not a directory: {}",
+                path.display()
+            );
+        } else if !directory_is_empty(path) {
+            bail!("Output directory is not empty: {}", path.display());
+        }
     }
     std::fs::create_dir_all(path).context("When creating output directory")
 }
