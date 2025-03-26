c6248806789714fc21f7d5d7ce18e943e8c3ea29
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-03-26 16:52:39 +0100

    Add Repository::commit_all

--- crates/aigi_git/src/lib.rs
+++ crates/aigi_git/src/lib.rs
@@ -4,37 +4,6 @@
 // use std::fs::File;
 // use std::io::Write;
 // use std::path::{Path, PathBuf};
-//
-// // fn clone_repo() {
-// //     let url = "git@github.com:jl-wynen/laikr.git";
-// //     let target = PathBuf::from("/home/jl/Prog/experiments/git-rs/laikr");
-// //     if target.exists() {
-// //         std::fs::remove_dir_all(&target).unwrap();
-// //     }
-// //
-// //     let git_config = git2::Config::open_default().expect("Failed to open git config");
-// //
-// //     // This requires a working auth method.
-// //     // It will not work in RustRover but does work in a terminal when my yubikey is plugged in
-// //     let mut callbacks = git2::RemoteCallbacks::new();
-// //     let mut cred_handler = git2_credentials::CredentialHandler::new(git_config);
-// //     callbacks.credentials(move |_url, username_from_url, _allowed_types| {
-// //         dbg!(_url, username_from_url, _allowed_types);
-// //         // let cred = git2::Cred::ssh_key_from_agent(username_from_url.unwrap());
-// //         let cred = cred_handler.try_next_credential(url, username_from_url, _allowed_types);
-// //         dbg!(cred.is_ok());
-// //         cred
-// //     });
-// //
-// //     let mut fo = git2::FetchOptions::new();
-// //     fo.remote_callbacks(callbacks);
-// //
-// //     let mut builder = git2::build::RepoBuilder::new();
-// //     builder.fetch_options(fo);
-// //
-// //     let repo = builder.clone(url, &target).unwrap();
-// // }
-//
 // fn add_and_commit_all(repo: &git2::Repository, message: &str) -> Result<()> {
 //     let sig = repo.signature()?;
 //     let parents = match repo.head() {
--- crates/aigi_git/src/repo.rs
+++ crates/aigi_git/src/repo.rs
@@ -1,12 +1,12 @@
 use anyhow::{Result, bail};
-use std::path::PathBuf;
+use std::path::Path;
 
 pub struct Repository {
     repo: git2::Repository,
 }
 
 impl Repository {
-    pub fn clone(url: &str, target: PathBuf, shallow: bool) -> Result<Self> {
+    pub fn clone(url: &str, target: &Path, shallow: bool) -> Result<Self> {
         if target.exists() {
             bail!("Output directory {} already exists", target.display());
         }
@@ -26,7 +26,52 @@ impl Repository {
 
         let repo = git2::build::RepoBuilder::new()
             .fetch_options(fetch_options)
-            .clone(url, &target)?;
+            .clone(url, target)?;
         Ok(Self { repo })
+    }
+
+    pub fn init(path: &Path) -> Result<Self> {
+        let repo = git2::Repository::init(path)?;
+        Ok(Self { repo })
+    }
+
+    /// Commit all changes to the repository and advance HEAD.
+    pub fn commit_all(&self, message: &str) -> Result<()> {
+        let mut index = self.add_all()?;
+        self.commit_to_head(message, &mut index)
+    }
+
+    /// Add all changes to the index.
+    fn add_all(&self) -> Result<git2::Index> {
+        let mut index = self.repo.index()?;
+        index.add_all(["*"].iter(), git2::IndexAddOption::DEFAULT, None)?;
+        index.write()?;
+        Ok(index)
+    }
+
+    /// Write current index and advance HEAD.
+    fn commit_to_head(&self, message: &str, index: &mut git2::Index) -> Result<()> {
+        let signature = self.repo.signature()?;
+        let parents = self.parents_from_head()?;
+        let new_tree_oid = index.write_tree()?;
+        let new_tree = self.repo.find_tree(new_tree_oid)?;
+        self.repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            message,
+            &new_tree,
+            // iter.collect to turn vec of values into vec of refs (there must be a better way!)
+            &parents.iter().collect::<Vec<_>>(),
+        )?;
+        Ok(())
+    }
+
+    /// Get parent commits for making a new commit based on current HEAD.
+    fn parents_from_head(&self) -> Result<Vec<git2::Commit>> {
+        Ok(match self.repo.head() {
+            Ok(head) => vec![self.repo.find_commit(head.target().unwrap())?],
+            Err(_) => vec![],
+        })
     }
 }
